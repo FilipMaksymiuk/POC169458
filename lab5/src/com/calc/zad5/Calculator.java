e0d686f05b031788d9719c0da0e06603ba597298
FilipMaksymiuk <115099709+FilipMaksymiuk@users.noreply.github.com>
2023-11-06 09:51:04 +0100

    Update Calculator.java

--- lab5/src/com/calc/zad5/Calculator.java
+++ lab5/src/com/calc/zad5/Calculator.java
@@ -1,28 +1,25 @@
 package com.calc.zad5;
 
 public class Calculator{
-    public static int dodawanie(int a,int b){
-        return a+b;
+    public static void dodawanie(int a,int b){
+        System.out.println(a+b);
     }
 
-    public static int odejmowanie(int a,int b){
-        return a-b;
+    public static void odejmowanie(int a,int b){
+        System.out.println(a-b);
+
     }
 
-    public static int mnozenie(int a,int b){
-        return a*b;
+    public static void mnozenie(int a,int b){
+        System.out.println(a*b);
     }
 
-    public static double dzielenie(double a,double b){
+    public static void dzielenie(double a,double b){
         if (b!=0){
-            return a/b;
+            System.out.println(a/b);
         } else{
            System.out.println("Dzielenie przez zero!");
-           return 0;
+
         }
     }
 }
-/*
-  Utwórz drugą klasę, w
-  której przetestujesz te metody bez tworzenia instancji
-klasy Calculator.*/
