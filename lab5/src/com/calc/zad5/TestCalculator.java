c7e9158f71660a146850fcaf323fa731fd2c9ff4
FilipMaksymiuk <115099709+FilipMaksymiuk@users.noreply.github.com>
2023-11-06 09:51:55 +0100

    Update TestCalculator.java

--- lab5/src/com/calc/zad5/TestCalculator.java
+++ lab5/src/com/calc/zad5/TestCalculator.java
@@ -5,10 +5,10 @@ public class TestCalculator {
         int a=10;
         int b=0;
 
-        System.out.println("Dodawanie: "+Calculator.dodawanie(a,b));
-        System.out.println("Odejmowanie: "+Calculator.odejmowanie(a,b));
-        System.out.println("Mnożenie: "+Calculator.mnozenie(a,b));
-        System.out.println("Dzielenie: "+Calculator.dzielenie(a,b));
+        Calculator.dodawanie(a,b);
+        Calculator.odejmowanie(a,b);
+        Calculator.mnozenie(a,b);
+        Calculator.dzielenie(a,b);
 
 
     }
